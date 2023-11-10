1549616ec4704a354cb199c950284a6a47531ac6
erikson1970 <leifhille@gmail.com>
2023-11-10 12:11:53 -0500

    remove random uneeded files

--- testing.py
+++
@@ -1,9 +0,0 @@
-# this is a standard hello world program that showcases print and input functions as well as loops and if statements
-nnn=0
-while nnn<5:
-    print(f"Hello World {nnn}")
-    nnn+=1
-
-# get user input here
-name=input("What is your name? ")
-print(f"Hello {name}!")
